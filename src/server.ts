73c5658bd512112f3c3a262f82a1c0db4ae8dd92
ashikurriyal <riyal.ashikur@gmail.com>
2025-07-02 18:21:33 +0600

    Initialize the repo with typescript and basic web server using express

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import app from "./app";
+
+let server;
+const port = 3000;
+const bootstrap = async () => {
+    server = app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+}
+
+bootstrap()
+
+//Server file; = Server handling like -> Server Starting, Closing, Error handling of server. Only related to server
